7dd37c374c23ba05377db59285c0ca146f8fa5ba
vovanlong <vovanlong1997@gmail.com>
2018-10-12 20:53:48 +0700

    fist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthTabsComponent } from './components/auth-tabs/auth-tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthTabsComponent
   ],
   imports: [
     BrowserModule
