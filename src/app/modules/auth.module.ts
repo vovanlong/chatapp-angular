45512f0bfcada6d75f05340841e5047c61f3b5bf
vovanlong <vovanlong1997@gmail.com>
2018-10-13 11:33:20 +0700

    layout login signup part done

--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { LoginComponent } from '../components/login/login.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AuthTabsComponent],
-  exports: [AuthTabsComponent]
+  declarations: [AuthTabsComponent, SignupComponent, LoginComponent],
+  exports: [AuthTabsComponent, SignupComponent, LoginComponent]
 })
 export class AuthModule {}
