fc84fc022cd19578084991e484a48bbf86818036
vovanlong <vovanlong1997@gmail.com>
2018-10-14 09:37:37 +0700

    success service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const BASEURL = 'http://localhost:3000/api/chatapp';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  registerUser(body): Observable<any> {
+    return this.http.post(`${BASEURL}/register`, body);
+  }
+}
