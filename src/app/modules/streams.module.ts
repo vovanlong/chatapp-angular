f03136b251b74f09f3091b396a110e2ec0dbcd03
vovanlong <vovanlong1997@gmail.com>
2018-10-15 19:12:06 +0700

    side component done

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { SideComponent } from '../components/side/side.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [StreamsComponent, ToolbarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent],
   exports: [StreamsComponent],
   providers: [TokenService]
 })
