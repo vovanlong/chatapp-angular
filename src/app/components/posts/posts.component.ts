6d4be0a8f4cc862705c269f146de1c9417db48c0
Long Vo <vovanlong1997@gmail.com>
2018-10-16 13:06:50 +0700

    install moment module done

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -18,5 +18,9 @@ export class PostsComponent implements OnInit {
       this.posts = data.posts;
       console.log(data);
     });
+  }
+
+  TimeFromNow(time) {
+    return moment(time).fromNow();
   }
 }
