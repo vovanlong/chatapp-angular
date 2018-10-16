0fb59c343de163d51e8e252f285188db76cbd25b
Long Vo <vovanlong1997@gmail.com>
2018-10-16 12:34:19 +0700

    get all post done

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private postService: PostService) {}
+  posts = [];
   ngOnInit() {
+    this.AllPost();
   }
 
+  AllPost() {
+    this.postService.getAllPosts().subscribe(data => {
+      this.posts = data.posts;
+      console.log(data);
+    });
+  }
 }
