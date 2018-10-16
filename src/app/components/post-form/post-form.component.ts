ce4e7999edaba1c3623a64f03d1cd9d4da100bf7
Long Vo <vovanlong1997@gmail.com>
2018-10-16 13:39:35 +0700

    client listening server by socketIO done

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PostService } from 'src/app/services/post.service';
-
+import io from 'socket.io-client';
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  socketHost: any;
+  socket: any;
   postForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private postService: PostService) {}
+  constructor(private fb: FormBuilder, private postService: PostService) {
+    this.socket = io('http://localhost:3000');
+  }
 
   ngOnInit() {
     this.init();
@@ -23,7 +27,8 @@ export class PostFormComponent implements OnInit {
   submitPost() {
     console.log(this.postForm.value);
     this.postService.addPost(this.postForm.value).subscribe(data => {
-      console.log(data);
+      // console.log(data);
+      this.socket.emit('refresh', {});
       this.postForm.reset();
     });
   }
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
 import * as moment from 'moment';
+import io from 'socket.io-client';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  constructor(private postService: PostService) {}
   posts = [];
+  socket: any;
+
+  constructor(private postService: PostService) {
+    this.socket = io('http://localhost:3000');
+  }
   ngOnInit() {
-    this.AllPost();
+    this.AllPosts();
+    this.socket.on('refreshPage', data => {
+      this.AllPosts();
+    });
   }
 
-  AllPost() {
+  AllPosts() {
     this.postService.getAllPosts().subscribe(data => {
       this.posts = data.posts;
       console.log(data);
