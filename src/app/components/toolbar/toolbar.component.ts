d81469e40c2684f279fd109c79c84ea8a88cb84d
Long Vo <vovanlong1997@gmail.com>
2018-10-27 14:01:55 +0700

    done

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -17,6 +17,8 @@ export class ToolbarComponent implements OnInit {
   socket: any;
   notifications = [];
   count = [];
+  chatList = [];
+  msgNumber = 0;
 
   constructor(private tokenService: TokenService, private router: Router, private usersService: UsersService) {
     this.socket = io('http://localhost:3000');
@@ -24,8 +26,15 @@ export class ToolbarComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.tokenService.GetPayload();
-    const dropdownElement = document.querySelector('.dropdown-trigger');
+    const dropdownElement = document.querySelectorAll('.dropdown-trigger');
     M.Dropdown.init(dropdownElement, {
+      alignment: 'right',
+      hover: true,
+      coverTrigger: false
+    });
+
+    const dropdownElementTwo = document.querySelectorAll('.dropdown-trigger1');
+    M.Dropdown.init(dropdownElementTwo, {
       alignment: 'right',
       hover: true,
       coverTrigger: false
@@ -34,6 +43,19 @@ export class ToolbarComponent implements OnInit {
     this.socket.on('refreshPage', () => {
       this.GetUser();
     });
+  }
+
+  ChecIfRead(arr) {
+    const checkArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const receiver = arr[i].msgId.message[arr[i].msgId.message.length - 1];
+      if (this.router.url !== `/chat/${receiver.sendername}`) {
+        if (receiver.isRead === false && receiver.receivername === this.user.username) {
+          checkArr.push(1);
+          this.msgNumber = _.sum(checkArr);
+        }
+      }
+    }
   }
 
   logout() {
@@ -47,6 +69,9 @@ export class ToolbarComponent implements OnInit {
         this.notifications = data.result.notifications.reverse() || '';
         const value = _.filter(this.notifications, ['read', false]);
         this.count = value;
+        this.chatList = data.result.chatList;
+        this.ChecIfRead(this.chatList);
+        console.log(this.msgNumber);
       },
       err => {
         if (err.error.token === null) {
@@ -69,5 +94,14 @@ export class ToolbarComponent implements OnInit {
   }
   TimeFromNow(time) {
     return moment(time).fromNow();
+  }
+
+  MessageDate(data) {
+    return moment(data).calendar(null, {
+      sameDay: '[Today]',
+      lastDay: '[YesterDay]',
+      lastWeek: 'DD/MM/YYYY',
+      sameElse: 'DD/MM/YYYY'
+    });
   }
 }
